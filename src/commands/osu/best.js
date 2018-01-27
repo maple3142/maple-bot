8b4d3fed1f66aed5ed5aa248bb638d1b0c4ac4d2
maple3142 <kirby741852963@gmail.com>
2018-01-27 20:48:36 +0800

    add osu best

--- src/commands/osu/best.js
+++ src/commands/osu/best.js
@@ -2,16 +2,16 @@ import client from '../../client'
 import * as osuapi from './api'
 import debug from 'debug'
 
-const log = debug('app:cmd:osu:recent')
+const log = debug('app:cmd:osu:best')
 
 export default async function stats([id, mode = 'std']) {
 	if (!id) return 'no id provided'
 	if (!(mode in osuapi.modemap)) return 'mode invaild'
 
-	const data = await osuapi.get_user_recent(id, mode)
+	const data = await osuapi.get_user_best(id, mode)
 
 	if (!data || data.length === 0) {
-		return 'Player not found or No recent found'
+		return 'Player not found or No best found'
 	} else {
 		const recents = await Promise.all(
 			data.filter(bmp => bmp).map(async bmp => ({
@@ -25,7 +25,7 @@ export default async function stats([id, mode = 'std']) {
 				rc =>
 					`${rc.beatmap.artist}-${rc.beatmap.title}[${
 						rc.beatmap.version
-					}] ${rc.rank}`
+					}] ${rc.rank} ${rc.pp}pp`
 			)
 			.join('\n\n')
 	}
