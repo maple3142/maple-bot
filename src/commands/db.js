af6447f0507cbfcecdbf24996b11badef6a02302
maple3142 <kirby741852963@gmail.com>
2018-03-25 00:23:20 +0800

    fix db

--- src/commands/db.js
+++ src/commands/db.js
@@ -10,12 +10,11 @@ export function handler(args, { isAdmin }) {
 	const [type, key, value] = args
 	if (type === 'get' && key) {
 		if (key === '*')
-			return db
-				.get('*')
+			return JSON.parse(db.get('*'))
 				.then(x => Object.keys(x).map(k => `${k}=${x[k]}`))
 				.then(s => s.join('\n'))
 		else return db.get(key).then(x => String(x))
-	} else if (type === 'set' && key && key !== '*' && value) return db.set(key, value)
+	} else if (type === 'set' && key && key !== '*' && value) return db.set(String(key), String(value))
 	else if (type === 'del' && key) return db.del(key)
 	else return description
 }
